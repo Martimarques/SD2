b88e72431287be812c8f8e219a9273e5d18ec54d
Ricardo <rid.ferreira@campus.fct.unl.pt>
2026-05-08 16:31:19 +0100

    Começo da implementação do TLS

--- src/sd2526/trab/impl/grpc/servers/AbstractGrpcServer.java
+++ src/sd2526/trab/impl/grpc/servers/AbstractGrpcServer.java
@@ -1,15 +1,22 @@
 package sd2526.trab.impl.grpc.servers;
 
-
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.security.KeyStore;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.net.ssl.KeyManagerFactory;
+
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyServerBuilder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+
 import sd2526.trab.impl.discovery.Discovery;
 import sd2526.trab.impl.java.servers.AbstractServer;
-import sd2526.trab.impl.utils.IP;
 
 
 public abstract class AbstractGrpcServer extends AbstractServer {
@@ -20,21 +27,60 @@ public abstract class AbstractGrpcServer extends AbstractServer {
 	protected final Server server;
 
 	protected AbstractGrpcServer(Logger log, String service, int port) {
-		super(log, service, String.format(SERVER_BASE_URI, IP.hostAddress(), port, GRPC_CTX));
-		
-		var builder = ServerBuilder.forPort(port);
-		for( var s : controllers( super.serverURI ) )
-			builder.addService( s );
-		
-		this.server = builder.build();
+		// O URI tem de usar o HostName em vez do IP para o TLS funcionar (como diz no PPT)
+		super(log, service, String.format(SERVER_BASE_URI, getHostname(), port, GRPC_CTX));
+
+		try {
+			// 1. Ler as propriedades da JVM (Keystore) do PPT
+			String keyStoreFilename = System.getProperty("javax.net.ssl.keyStore");
+			String keyStorePassword = System.getProperty("javax.net.ssl.keyStorePassword");
+
+			// 2. Carregar a Keystore
+			KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+			try(FileInputStream input = new FileInputStream(keyStoreFilename)) {
+				keystore.load(input, keyStorePassword.toCharArray());
+			}
+
+			// 3. Inicializar a KeyManagerFactory
+			KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(
+					KeyManagerFactory.getDefaultAlgorithm());
+			keyManagerFactory.init(keystore, keyStorePassword.toCharArray());
+
+			// 4. Criar o Contexto SSL
+			SslContext context = GrpcSslContexts.configure(
+					SslContextBuilder.forServer(keyManagerFactory)
+			).build();
+
+			// 5. Usar NettyServerBuilder com o SslContext injetado
+			var builder = NettyServerBuilder.forPort(port).sslContext(context);
+
+			for( var s : controllers( super.serverURI ) ) {
+				builder.addService( s );
+			}
+
+			this.server = builder.build();
+
+		} catch (Exception e) {
+			log.severe("Falhou a inicialização do TLS/gRPC: " + e.getMessage());
+			throw new RuntimeException("Erro TLS no gRPC", e);
+		}
+	}
+
+	// Método auxiliar para ir buscar o Hostname
+	private static String getHostname() {
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (Exception e) {
+			return "localhost";
+		}
 	}
 
 	protected abstract List<GrpcController> controllers( String uri );
-	
+
 	protected void start() throws IOException {
-		
+
 		Discovery.getInstance().announce(serviceName(), super.serverURI);
-		
+
 		Log.info(String.format("%s gRPC Server ready @ %s\n", service, serverURI));
 
 		server.start();
@@ -44,5 +90,4 @@ public abstract class AbstractGrpcServer extends AbstractServer {
 			System.err.println("*** server shut down");
 		}));
 	}
-	
 }
--- src/sd2526/trab/impl/rest/servers/AbstractRestServer.java
+++ src/sd2526/trab/impl/rest/servers/AbstractRestServer.java
@@ -1,7 +1,9 @@
 package sd2526.trab.impl.rest.servers;
 
+import java.net.InetAddress;
 import java.net.URI;
 import java.util.logging.Logger;
+import javax.net.ssl.SSLContext; // Importação necessária para o TLS
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -10,29 +12,44 @@ import sd2526.trab.impl.discovery.Discovery;
 import sd2526.trab.impl.java.servers.AbstractServer;
 import sd2526.trab.impl.utils.IP;
 
-
-
 public abstract class AbstractRestServer extends AbstractServer {
-	private static final String SERVER_BASE_URI = "http://%s:%s%s";
+	private static final String SERVER_BASE_URI = "https://%s:%s%s";
 	private static final String REST_CTX = "/rest";
+	private final int port;
 
 	protected AbstractRestServer(Logger log, String service, int port) {
-		super(log, service, String.format(SERVER_BASE_URI, IP.hostAddress(), port, REST_CTX));
+		super(log, service, String.format(SERVER_BASE_URI, getHostnameLocal(), port, REST_CTX));
+		this.port = port;
+	}
+
+	private static String getHostnameLocal() {
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "localhost";
+		}
 	}
 
 	protected void start() {
-		
-		ResourceConfig config = new ResourceConfig();
-		
-		registerResources( config );
-		
-		JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(IP.hostAddress(), INETADDR_ANY)), config);
-		
-		if( service != null )
-			Discovery.getInstance().announce(serviceName(), super.serverURI);
-		
-		Log.info(String.format("%s Server ready @ %s\n",  service, serverURI));
+		try {
+			ResourceConfig config = new ResourceConfig();
+
+			registerResources( config );
+
+			URI bindUri = URI.create(String.format(SERVER_BASE_URI, INETADDR_ANY, port, REST_CTX));
+
+			JdkHttpServerFactory.createHttpServer(bindUri, config, SSLContext.getDefault());
+
+			if( service != null )
+				Discovery.getInstance().announce(serviceName(), super.serverURI);
+
+			Log.info(String.format("%s Server ready @ %s\n",  service, serverURI));
+
+		} catch (Exception e) {
+			Log.severe("Failed to start server: " + e.getMessage());
+		}
 	}
-	
+
 	abstract void registerResources( ResourceConfig config );
 }
